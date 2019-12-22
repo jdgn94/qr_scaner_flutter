e865e8ed98701f91aa6cd396e7e84a2aa82c6d2b
juan <jdgn94@gmail.com>
2019-12-22 12:34:23 -0400

    implementacion del bottomNavigationBar y las 2 paginas a navegar con el

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,53 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:qr_scaner/src/pages/maps_page.dart';
+import 'package:qr_scaner/src/pages/urls_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int actualPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Pagina principal'),
-      ),
+      body: _callPage(actualPage),
+      bottomNavigationBar: _bottomNavigationBar(),
     );
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      onTap: (i) {
+        setState(() {
+          actualPage = i;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.map),
+          title: Text('Mapas'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.web),
+          title: Text('URLs'),
+        ),
+      ],
+    );
+  }
+
+  Widget _callPage(int actualPage) {
+    switch (actualPage) {
+      case 0:
+        return MapsPage();
+      case 1:
+        return UrlsPage();
+      default:
+        return MapsPage();
+    }
   }
 }
---
+++ lib/src/pages/urls_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UrlsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('URLs pagina'),
+    );
+  }
+}
