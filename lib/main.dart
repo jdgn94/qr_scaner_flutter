8464a66420b9353c4ecb19d32ae96a024ed948ae
juan <jdgn94@gmail.com>
2019-12-22 11:49:40 -0400

    archivos bases del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:qr_scaner/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage(),
+      },
+    );
+  }
+}
